4bb13a01f951ff2c17dbdce9f5511c34665ceca5
dicusar <dicusar.dv@gmail.com>
2022-07-31 15:29:25 +0300

    Create a program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace GitLearn;
+
+static class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("Two number sum calculator");
+        GetInput("Enter the first value: ", out int firstValue);
+        GetInput("Enter the second value: ", out int secondValue);
+        var result = firstValue + secondValue;
+        Console.WriteLine($"{firstValue} + {secondValue} = {result}");
+    }
+
+    private static void GetInput(string message, out int value)
+    {
+        Console.Write(message);
+        _ = int.TryParse(Console.ReadLine(), out value);
+    }
+}
