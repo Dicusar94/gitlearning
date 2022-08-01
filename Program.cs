60d063f90ae8dec5fee1a1af8ff383cb6fe84d0c
dicusar <dicusar.dv@gmail.com>
2022-08-01 10:17:40 +0300

    Created Calculator Manage

--- Program.cs
+++ Program.cs
@@ -4,25 +4,14 @@ static class Program
 {
     private static void Main()
     {
-        Console.WriteLine("Two number sum calculator");
-        GetInput("Enter the first value: ", out int firstValue);
-        GetInput("Enter the second value: ", out int secondValue);
-        var result = firstValue + secondValue;
-        Console.WriteLine($"{firstValue} + {secondValue} = {result}");
-
+        CalculatorManager.Addition();
         SecondProgram();
-    }
-
-    private static void GetInput(string message, out int value)
-    {
-        Console.Write(message);
-        _ = int.TryParse(Console.ReadLine(), out value);
     }
 
     private static void SecondProgram()
     {
         Console.WriteLine("Hello, World!");
-        Console.Write("What is your date of birth");
+        Console.Write("What is your date of birth : ");
         var isDate = DateTime.TryParse(Console.ReadLine(), out DateTime dateOfBirth);
         if (isDate)
         {
